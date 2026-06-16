c5cc32f0462ab5911a5e259a8544422ad5bed9ac
Ephraim KINDA <ephraim@example.com>
2026-06-16 15:53:01 +0000

    Add SSL support for PlanetScale/remote MySQL

--- afrijob_backend/config/database.js
+++ afrijob_backend/config/database.js
@@ -6,6 +6,7 @@ const pool = mysql.createPool({
     user:     process.env.DB_USER     || 'root',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME     || 'bddiane_sp',
+    ssl:      { rejectUnauthorized: false }, // Support PlanetScale/Remote MySQL
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
